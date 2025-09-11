54fb2e7c99f617d80eaa45d5c1b4228380ce8da0
mykim-dev <pandagom79@gmail.com>
2025-09-12 00:17:49 +0900

    Updates

--- _next/static/chunks/c4822c9e1647e25a.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/a4865e00802f9789.js",
-  "static/chunks/48757f0b44f4a931.js",
-  "static/chunks/turbopack-edb9edff9df21e66.js"
-])
---
+++ _next/static/chunks/ffcf302cc529aea1.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/9ff5f377d08cd1b3.js",
+  "static/chunks/c5690e4e9434cefd.js",
+  "static/chunks/turbopack-c142b1b50d00e809.js"
+])
