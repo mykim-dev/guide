9640dfcec68ff7e8aef86b55ca6e92ed263bcebf
my.kim <my.kim@quintet.co.kr>
2025-09-11 10:21:42 +0900

    스타일적용되지 않음

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/guide',
+  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true
